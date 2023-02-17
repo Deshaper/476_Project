4aa8a10584175e0656b9e796908fe1cc5390cd9f
Deshaper <dexinsuoyine@gmail.com>
2023-02-16 20:26:27 -0700

    添加项目文件。

---
+++ Data/MVC476Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC476.Models;
+
+namespace MVC476.Data
+{
+    public class MVC476Context : DbContext
+    {
+        public MVC476Context (DbContextOptions<MVC476Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC476.Models.Test> Test { get; set; } = default!;
+    }
+}
---
+++ Models/Seeddata.cs
@@ -0,0 +1,64 @@
+﻿//using Microsoft.EntityFrameworkCore;
+
+//namespace MVC476.Models
+//{
+  //  public class Seeddata
+    //{
+    //}
+//}
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVC476.Data;
+using System;
+using System.Linq;
+
+namespace MVC476.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MVC476Context(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MVC476Context>>()))
+        {
+            // Look for any movies.
+            if (context.Test.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Test.AddRange(
+                new Test
+                {
+                    name = "When Harry Met Sally",
+                    BirthDay = DateTime.Parse("1989-2-12"),
+                    description = "Romantic Comedy",
+                    status = 7
+                },
+                new Test
+                {
+                    name = "Ghostbusters ",
+                    BirthDay = DateTime.Parse("1984-3-13"),
+                    description = "Comedy",
+                    status = 8
+                },
+                new Test
+                {
+                    name = "Ghostbusters 2",
+                    BirthDay = DateTime.Parse("1986-2-23"),
+                    description = "Comedy",
+                    status = 9
+                },
+                new Test
+                {
+                    name = "Rio Bravo",
+                    BirthDay = DateTime.Parse("1959-4-15"),
+                    description = "Western",
+                    status = 3
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
