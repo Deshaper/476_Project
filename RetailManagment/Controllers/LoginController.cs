2ed8a0b594468a0a52ac51e6fa6e981a5784eb8e
Deshaper <dexinsuoyine@gmail.com>
2023-03-08 09:07:51 -0700

    Update Login functionality and Session
    Add Login functionality with validate by match data in database in the controller and Start Session by Store Name into Session.
    Add functionality, the layout would represent the Name from session, if there is no user logged in, present Guest instead of any name

--- RetailManagment/Controllers/LoginController.cs
+++ RetailManagment/Controllers/LoginController.cs
@@ -3,21 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
 using RetailManagment.Models;
 using RetailManagment.Data;
+using System.ComponentModel.DataAnnotations;
 
 namespace RetailManagment.Controllers
 {
    
     public class LoginController : Controller
-    { 
+    {
+        private readonly string _connectionString = "data source=LAPTOP-SNS0CLD2;initial catalog=Retail_management;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
         private Model1 db = new Model1();
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
         // GET: Login
         [HttpPost]
-        public ActionResult Login(string Username, string Password)
+        public ActionResult Login(Customer customer)
         {
+            //string namequery = "SELECT * FROM Customer WHERE  Customer.Username = Username AND Customer.Password = Password";
 
+           // SqlCommand sql1= new SqlCommand(namequery, db);
 
+            if(!ModelState.IsValid)
+            {
+                return View(customer);
+            }
+
+            string query = "SELECT Name FROM Customer WHERE Email = @Email AND Password = @Password";
+            using (SqlConnection sqlConnection = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, sqlConnection);
+                // assign data post by login to variable that use for query
+                cmd.Parameters.AddWithValue("@Email", customer.Email);
+                cmd.Parameters.AddWithValue("@Password", customer.Password);
+
+                // connect to database and execute the query
+                sqlConnection.Open();
+                //Start reader to read the cmd result
+                SqlDataReader reader = cmd.ExecuteReader(); 
+                // Validate if there is Matching data from database
+                if(reader.HasRows)
+                {
+                    reader.Read();
+                    string name = reader.GetString(0);
+
+                    // Automatically start session
+                    Session["Name"] = name;
+
+                    return RedirectToAction("Index","Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Error Email and Error Password");
+                    return View(customer);
+                }
+            }
+
+            
         }
     }
 }
