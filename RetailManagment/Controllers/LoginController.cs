02356c678c4799232bf589d3e6e6376d15e560b4
Deshaper <dexinsuoyine@gmail.com>
2023-03-07 17:32:35 -0700

    Update Migration
    Update Migrations, use migration to update the database, but make sure u have a connected database in sql, so u can easily update the database use the migration

---
+++ RetailManagment/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RetailManagment.Models;
+using RetailManagment.Data;
+
+namespace RetailManagment.Controllers
+{
+   
+    public class LoginController : Controller
+    { 
+        private Model1 db = new Model1();
+        // GET: Login
+        [HttpPost]
+        public ActionResult Login(string Username, string Password)
+        {
+
+
+        }
+    }
+}
