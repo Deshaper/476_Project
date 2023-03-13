589e51cd4fb669efe6659c33606e774e0ddb36bd
Deshaper <dexinsuoyine@gmail.com>
2023-03-12 19:09:55 -0600

    Update Cart page
    Update Cart Page, add functionality to view cart content of logged in user, and able to turn to Product_detail page by click on detail of each item in the cart

---
+++ RetailManagment/Controllers/CartController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RetailManagment.Data;
+using RetailManagment.Models;
+using RetailManagment.ViewModels;
+
+namespace RetailManagment.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly string _connectionString = "data source=LAPTOP-SNS0CLD2;initial catalog=Retail_management;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
+        private Model1 db = new Model1();
+
+
+        // GET: Cart/CarList
+        public ActionResult CartList()
+        {
+            List<Particular_cart> model2 = new List<Particular_cart>();
+
+            var nameuse = Session["name"];
+            var Cusid = db.Customers.FirstOrDefault(c => c.Name == nameuse);
+
+
+            string q2 = "SELECT Product_name, Quantity, Cus_id FROM Cart WHERE Cus_id = @Cusid";
+
+            using(SqlConnection sqlConnection = new SqlConnection(_connectionString))
+            {
+ 
+                SqlCommand cmd = new SqlCommand(q2, sqlConnection);
+                cmd.Parameters.AddWithValue("@Cusid", Cusid.Cus_id);
+
+                sqlConnection.Open();
+
+                using (SqlDataReader read = cmd.ExecuteReader())
+                { 
+                    while (read.Read())
+                    {
+                        Particular_cart cart1 = new Particular_cart();
+
+                        cart1.Product_name = (string)read["Product_name"];
+                        cart1.Quantity = (int)read["Quantity"];
+                        cart1.Cus_id = (int)read["Cus_id"];
+
+                        model2.Add(cart1);
+                        
+
+                    }
+                }
+                    
+
+            }
+            return View(model2);
+        }
+
+        // GET: Cart/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cart/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cart/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Cart/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cart/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Cart/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
