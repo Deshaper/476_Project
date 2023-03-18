cd39c2d8e36e0f25223f483074d56fbbd3870fed
Deshaper <dexinsuoyine@gmail.com>
2023-03-18 15:32:37 -0600

    Finish Inventory Management
    Finish the Inventory management part, check inventory level for every access, Display alerts message in the top of page. Include inventory product detail. Update in page.

---
+++ RetailManagment/ViewModels/Inven_alert.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RetailManagment.ViewModels
+{
+    public class Inven_alert
+    {
+       public List<string> Inven_item { get; set; }
+        public List<string> Alerts { get; set; }
+    }
+}
