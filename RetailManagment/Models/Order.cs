36fde877431986273392d4dcbf0766a0e2c6e3c7
Surfer299 <122629913+Surfer299@users.noreply.github.com>
2023-03-20 20:20:22 -0600

    Sales and Employee first Checkin (Issues with system)

---
+++ RetailManagment/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿namespace RetailManagment.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Orders")]
+    public partial class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        public decimal TotalAmount { get; set; }
+
+        //public virtual Customer Customer { get; set; }
+    }
+}
